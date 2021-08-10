b8539758fe70c66f85f4a37c24291396e7f870d4
dirkzukunft <git@qf3.de>
2021-08-10 18:26:36 +0200

    Implement credential API

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,22 @@
 import express from 'express';
+import { getCredential } from './credentials';
 const app = express();
 const port = 3000;
 
+app.get('/api/credentials/:service', async (req, res) => {
+  const credential = await getCredential(req.params.service);
+
+  // if (credential) {
+  res.status(200).json(credential);
+  // } else {
+  //   res.status(404).send();
+  // }
+});
+
 app.get('/', (_req, res) => {
-  res.send('Hello World!');
+  res.send('');
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Listening at http://localhost:${port}`);
 });
