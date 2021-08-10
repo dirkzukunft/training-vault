6c76ddd0f6aef9c4e40ad62feb2ad53755fc5649
dirkzukunft <git@qf3.de>
2021-08-10 16:36:46 +0200

    Create basic express server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-console.log('Hello Server!');
+import express from 'express';
+const app = express();
+const port = 3000;
+
+app.get('/', (_req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
