c947ed4aa50320709008daccc5c73894875d468c
dirkzukunft <git@qf3.de>
2021-08-11 12:08:22 +0200

    Add API functionality

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,31 @@
 import express from 'express';
-import { getCredential } from './credentials';
+import { getCredential, readCredentials } from './credentials';
 const app = express();
 const port = 3000;
 
 app.get('/api/credentials/:service', async (req, res) => {
-  const credential = await getCredential(req.params.service);
+  const { service } = req.params;
+  try {
+    const credential = await getCredential(service);
+    res.status(200).json(credential);
+  } catch (error) {
+    res.status(404).send(`Credentials for ${service} not found`);
+    console.log(error);
+  }
+});
 
-  // if (credential) {
-  res.status(200).json(credential);
-  // } else {
-  //   res.status(404).send();
-  // }
+app.get('/api/credentials/', async (_req, res) => {
+  try {
+    const credentials = await readCredentials();
+    res.status(200).json(credentials);
+  } catch (error) {
+    res.status(500).send('No credentials found');
+    console.log(error);
+  }
 });
 
 app.get('/', (_req, res) => {
-  res.send('');
+  res.send('Hello');
 });
 
 app.listen(port, () => {
