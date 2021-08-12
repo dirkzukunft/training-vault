3352e4f47557367c47f343182145459e9dd2970e
dirkzukunft <git@qf3.de>
2021-08-12 18:29:21 +0200

    Add getAndCheckMasterPassword

---
+++ src/uitls/auth.ts
@@ -0,0 +1,13 @@
+import { validateMasterPassword } from './validation';
+
+export async function getAndCheckMasterPassword(req: any, res: any): Promise<string> {
+  const masterPassword = req.headers.authorization;
+  if (!masterPassword) {
+    res.status(400).send('Authorization header missing');
+    return '';
+  } else if (!(await validateMasterPassword(masterPassword))) {
+    res.status(401).send('Unauthorized request');
+    return '';
+  }
+  return masterPassword;
+}
