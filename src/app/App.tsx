ec97e399a6ce716735c048ba63bba846eb67dc13
dirkzukunft <git@qf3.de>
2021-08-19 11:06:35 +0200

    Apply Router to App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 
 function App(): JSX.Element {
-  return <Dashboard />;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Dashboard />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
