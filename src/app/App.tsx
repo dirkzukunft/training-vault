52c3e8a8672fbf7f8679a51091a54d13f79f12d8
dirkzukunft <git@qf3.de>
2021-08-20 19:14:06 +0200

    Add Headline in App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,7 +2,8 @@ import React, { useState } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Dashboard from './pages/Dashboard/Dashboard';
 import Password from './pages/Password/Password';
-import { MasterPasswordContext } from './components/MasterPasswordContext';
+import { MasterPasswordContext } from './components/MasterPasswordContext/MasterPasswordContext';
+import Headline from './components/Headline/Headline';
 
 function App(): JSX.Element {
   const [masterPassword, setMasterPassword] = useState<string>('no pw');
@@ -11,6 +12,7 @@ function App(): JSX.Element {
     <MasterPasswordContext.Provider
       value={{ masterPassword, setMasterPassword }}
     >
+      <Headline>Vault3</Headline>
       <BrowserRouter>
         <Switch>
           <Route exact path="/">
