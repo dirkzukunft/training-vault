8ddc1a8cd78230e001ab9a47988fd0bb8ee5161e
dirkzukunft <git@qf3.de>
2021-08-22 22:40:50 +0200

    Remove content-type

--- src/app/pages/DeleteCredential/DeleteCredential.tsx
+++ src/app/pages/DeleteCredential/DeleteCredential.tsx
@@ -12,7 +12,6 @@ export default function DeleteCredential(): JSX.Element {
       method: `DELETE`,
       headers: {
         Authorization: masterPassword,
-        'Content-Type': 'application/json',
       },
     });
 
