99a129caca69d545d2423cbab8ecda485013abc1
dirkzukunft <git@qf3.de>
2021-08-22 13:07:09 +0200

    Remove password.tsx

--- src/app/pages/Password/Password.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-
-export default function Password(): JSX.Element {
-  const { service } = useParams<{ service: string }>();
-  return <div>My URL parameter is {service}</div>;
-}
